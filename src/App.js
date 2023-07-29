479f89728a8c4790d81c15d6ad82b7dd544e7353
hiiam-andy <andrew.citb@gmail.com>
2023-07-29 18:33:15 +0300

    фикс

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./pages/AppRouter";
 
-import "../src/css/normalize.css";
+import "../src/style/normalize.css";
 import "../src/font/style-font.css";
-import "../src/css/style.css";
+import "../src/style/style.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
